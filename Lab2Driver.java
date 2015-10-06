6672178ea139f694878fb5c5bed1f6fa1d859e34
Matt Versaggi <mversagg@calpoly.edu>
2015-10-05 18:16:20 -0700

    Modified some stuff

--- Lab2Driver.java
+++ Lab2Driver.java
@@ -40,8 +40,7 @@ public class Lab2Driver
             if(input.equals("1"))
             {
                 System.out.print("Enter the name of the JSON file you wish to load from: ");
-                //input = reader.nextLine();
-                parse = new Parser(new File("fakeData.json"));
+                input = reader.nextLine();
             }
             //2 is dot product
             else if(input.equals("2"))
